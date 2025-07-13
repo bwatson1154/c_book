59070bd7b87ab9e6078d5e92d5d3ee4891a64d17
bwats <bwatson1154@icloud.com>
2025-07-13 20:03:16 +0000

    added exercises from ch3

---
+++ ch3/exercises/e01.c
@@ -0,0 +1,12 @@
+/*
+What output do the following calls of printf produce?
+(a) printf("%6d,%4d", 86, 1040);
+(b) printf("%12.5e", 30.253);
+(c) printf("%.4f", 83.162);
+(d) printf("%-6.2g", .0000009979);
+
+(a) should produce:    86,1040
+(b) should produce:  3.02530e+2
+(c) should produce:83.1620
+(d) should produce:.00   
+*/
